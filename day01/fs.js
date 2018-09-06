b548e92f18bb04d5f305a73af5cd7962bbd53d30
185096473 <185096473@qq.com>
2018-09-06 13:43:06 +0800

    fs

---
+++ day01/fs.js
@@ -0,0 +1,8 @@
+var http=require("http");
+var fs=require("fs");
+http.createServer((req,res)=>{
+    fs.readFile("./note.md",(err,data)=>{
+        res.writeHead(200,{"Content-type":"text/html;charset=utf-8"})
+        res.end(data)
+    })
+}).listen(3000,"127.0.0.1")
